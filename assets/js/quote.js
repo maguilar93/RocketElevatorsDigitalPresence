afa072a9fef53fc18301223b03d5c63b270af3c3
Maria Aguilar <maguilarc@protonmail.com>
2020-01-31 10:51:36 -0500

    Calculations for commercial and residential form is done.

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,3 +1,5 @@
+// Show fields depending on building type //
+
 let building = document.querySelector("#building");
 let residential = document.querySelector("#residentialForm");
 let commercial = document.querySelector("#commercialForm");
@@ -33,3 +35,39 @@ building.addEventListener("change", function() {
         corporate.style.display = "none";
     }
 });
+
+// Get number of elevators for commercial // 
+
+let elevatorCom = document.getElementsByName("contact[elevatorCom]")[0];
+let cages = document.getElementsByName("contact[cages]")[0];
+cages.addEventListener('input', function() {
+    elevatorCom.value = cages.value;
+});
+
+
+// Get number of elevators for residential // 
+
+ 
+function elevatorResidential() {
+
+    let apartments = document.getElementsByName("contact[apartments]")[0];
+    let floors = document.getElementsByName("contact[floors]")[0];
+    let elevatorRes = document.getElementsByName("contact[elevatorRes]")[0];
+
+    let avgDoors = parseInt(apartments.value)/parseInt(floors.value);
+    let numElevators = avgDoors/6;
+    let numColumns = Math.ceil(floors.value/20);
+    
+
+    if (apartments.value.length > 0 && floors.value.length > 0) {
+
+        if (floors.value <= 20) {
+            elevatorRes.value = Math.ceil(numElevators) * numColumns;
+        } else {
+            console.log(Math.ceil(numElevators))
+            elevatorRes.value = Math.ceil(numElevators)* numColumns;
+        }
+        
+}};
+
+
